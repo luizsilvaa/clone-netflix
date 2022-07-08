5788817b9c275f4b391192fa1aaa07855aafb772
mateusalvesc <mateus.alvesc02@gmail.com>
2022-07-08 13:32:06 -0300

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+export default ({black}) => {
+    return (
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1024px-Netflix_2015_logo.svg.png" alt="Netflix" />
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="/">
+                    <img src="https://noirflix.netlify.app/imgs/icon1.png" alt="Usuário" />
+                </a>
+            </div>
+        </header>
+    );
+}
